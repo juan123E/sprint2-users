e184a5adfd866c58126ae56e7a6b7d2eb3f0dc26
juan123E <j.quirogar@uniandes.edu.co>
2024-10-17 14:05:55 -0500

    login

--- users/users/urls.py
+++ users/users/urls.py
@@ -17,10 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
+
 def redirigir_a_login(request):
     return redirect('/login/')
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',redirigir_a_login),
     path('api/',include('usuarios.urls')),
     path('',include('django.contrib.auth.urls'))
 ]
