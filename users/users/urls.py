429b7c0300bcd3c36156a01f50612f52d784440b
juan123E <j.quirogar@uniandes.edu.co>
2024-10-17 11:42:17 -0500

    solucion error

--- users/users/urls.py
+++ users/users/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('usuarios.urls'))
+    path('api/',include('usuarios.urls')),
+    path('',include('django.contrib.auth.urls'))
 ]
