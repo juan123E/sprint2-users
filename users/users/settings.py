249549a8e8fcd76fffad014e3f1114028a580d93
juan123E <j.quirogar@uniandes.edu.co>
2024-10-18 16:27:49 -0500

    hi

--- users/users/settings.py
+++ users/users/settings.py
@@ -127,7 +127,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'usuarios.Usuario'
-LOGIN_REDIRECT_URL = '/usuarios'
+LOGIN_REDIRECT_URL = '/vistas/usuarios'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': []
 }
